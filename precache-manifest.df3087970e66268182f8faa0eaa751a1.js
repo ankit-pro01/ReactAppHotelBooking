6f6d86d4f375aa0f202107e1f8b9897064f8f673
ankit-pro01 <anku.emotiondigital@gmail.com>
2020-03-08 18:31:13 +0530

    some changes

---
+++ precache-manifest.df3087970e66268182f8faa0eaa751a1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdef1e6561f2ff9dff92374d59eac75",
+    "url": "/ReactAppHotelBooking/index.html"
+  },
+  {
+    "revision": "f175950e30f576b135aa",
+    "url": "/ReactAppHotelBooking/static/css/main.7789423d.chunk.css"
+  },
+  {
+    "revision": "1cdedc76b92e8565d5d6",
+    "url": "/ReactAppHotelBooking/static/js/2.266916a3.chunk.js"
+  },
+  {
+    "revision": "f175950e30f576b135aa",
+    "url": "/ReactAppHotelBooking/static/js/main.54748519.chunk.js"
+  },
+  {
+    "revision": "a9e1e9d9ad0ab4ea38e6",
+    "url": "/ReactAppHotelBooking/static/js/runtime~main.aeccf14b.js"
+  },
+  {
+    "revision": "8d54e7f5e75c90fc9f246193da3a4e8d",
+    "url": "/ReactAppHotelBooking/static/media/CoolSky.8d54e7f5.jpg"
+  },
+  {
+    "revision": "cc96c62d0c860959fabb87a480dd42ec",
+    "url": "/ReactAppHotelBooking/static/media/Login.cc96c62d.svg"
+  },
+  {
+    "revision": "a00fbf02279412bb01b9f80981529746",
+    "url": "/ReactAppHotelBooking/static/media/booked.a00fbf02.svg"
+  },
+  {
+    "revision": "1922b6d0b96b51e1e4512c6c725721de",
+    "url": "/ReactAppHotelBooking/static/media/cart.1922b6d0.svg"
+  },
+  {
+    "revision": "a9402345d5e2b39280985ec570b12093",
+    "url": "/ReactAppHotelBooking/static/media/forgot_password.a9402345.svg"
+  },
+  {
+    "revision": "72fa04b9d4d89de0fadf83c33d377d7e",
+    "url": "/ReactAppHotelBooking/static/media/landingImage3.72fa04b9.jpg"
+  },
+  {
+    "revision": "1a71cf95888195ce44cd523f5a2e5476",
+    "url": "/ReactAppHotelBooking/static/media/resort-icon.1a71cf95.png"
+  },
+  {
+    "revision": "a9055cc20fb8b6b781d69a2da2c67c86",
+    "url": "/ReactAppHotelBooking/static/media/roadSign.a9055cc2.svg"
+  },
+  {
+    "revision": "58ade6eda0c5561e3ce5b7f5572d55ef",
+    "url": "/ReactAppHotelBooking/static/media/room1.58ade6ed.jpg"
+  },
+  {
+    "revision": "4fa8e4bb66061a50501df209dedb4d92",
+    "url": "/ReactAppHotelBooking/static/media/room10.4fa8e4bb.jpg"
+  },
+  {
+    "revision": "20babcf0a6f856fc2d5a5d9cd3495766",
+    "url": "/ReactAppHotelBooking/static/media/room11.20babcf0.jpg"
+  },
+  {
+    "revision": "a04b287f77b6ab09b09000ad34e5cd43",
+    "url": "/ReactAppHotelBooking/static/media/room13.a04b287f.jpg"
+  },
+  {
+    "revision": "a49c65f09f925c1c78531b9c853700b8",
+    "url": "/ReactAppHotelBooking/static/media/room14.a49c65f0.jpg"
+  },
+  {
+    "revision": "1f521317cd48015645daa0a2d953dc2d",
+    "url": "/ReactAppHotelBooking/static/media/room15.1f521317.jpg"
+  },
+  {
+    "revision": "fb2019fb5bac36967bc0239873ade607",
+    "url": "/ReactAppHotelBooking/static/media/room2.fb2019fb.jpg"
+  },
+  {
+    "revision": "340252704b21111fef19a87b0e508e39",
+    "url": "/ReactAppHotelBooking/static/media/room3.34025270.jpg"
+  },
+  {
+    "revision": "69fcba87f94f9accf18b014f8d8e0212",
+    "url": "/ReactAppHotelBooking/static/media/room4.69fcba87.jpg"
+  },
+  {
+    "revision": "ca23fd38ac5f18f9da3e3bdb493c3a4d",
+    "url": "/ReactAppHotelBooking/static/media/room5.ca23fd38.jpg"
+  },
+  {
+    "revision": "6b254cb4be7c9390e0fd161d5e4fd915",
+    "url": "/ReactAppHotelBooking/static/media/room6.6b254cb4.jpg"
+  },
+  {
+    "revision": "ceb1a17d23bf564723c220356234e2cb",
+    "url": "/ReactAppHotelBooking/static/media/room7.ceb1a17d.jpg"
+  },
+  {
+    "revision": "72793ffa62f2999937774d7a464e48f3",
+    "url": "/ReactAppHotelBooking/static/media/room8.72793ffa.jpg"
+  },
+  {
+    "revision": "415ee50699412f67824a7da2b064aca5",
+    "url": "/ReactAppHotelBooking/static/media/room9.415ee506.jpeg"
+  },
+  {
+    "revision": "48716954311fecca0974e1a07d5996e9",
+    "url": "/ReactAppHotelBooking/static/media/travelPlans.48716954.svg"
+  }
+]);
