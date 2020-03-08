cc10dd4bcbb3b987b290608dd5a7c0f8cdec3f75
ankit-pro01 <anku.emotiondigital@gmail.com>
2020-03-08 20:11:10 +0530

    changes in router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
@@ -15,9 +15,9 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 const app = (
 
         <Provider store = {store}>
-            <BrowserRouter basename = {process.env.PUBLIC_URL}>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
 ); 
 
